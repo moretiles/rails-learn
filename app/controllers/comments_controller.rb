82afff0a002be760e718ef25fe2f87c0d1188545
David Vergano <verganodavid29@gmail.com>
2025-11-13 20:06:35 -0500

    Trying out Turbo Streams in Turbo-Rails

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,18 +7,33 @@ class CommentsController < ApplicationController
 
   def new
     @comment = Comment.new(product_id: params[:product_id])
+    request.variant = determine_variant
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to Product.find(@comment.product_id)
-    else
-      render :new, status: :unprocessable_entity
+    @comment = Comment.create!(comment_params)
+
+    respond_to do |format|
+      format.html do
+        redirect_to @comment.product
+      end
+
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append("comments", partial: "comments/index_element", locals: { comment: @comment })
+        #render turbo_stream: turbo_stream.refresh(request_id: nil)
+      end
     end
   end
 
   private
+    def determine_variant
+      if turbo_frame_request?
+        return :turbo_frame
+      else
+        return :html
+      end
+    end
+
     def set_comment
       @comment = Comment.find(params[:id])
     end
